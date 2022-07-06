ee0cd31e2bc36f60062a8cdf21b79cb4bf98c642
trevorregister <trevor.register@pivotinteractives.com>
2022-07-06 00:31:09 -0400

    sends html

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const path = require('path')
 
 router.get('/', (req, res)=>{
-    res.render('pages/index')
+    res.sendFile('C:/Users/trevo/Documents/Projects/weather-calendar/weather-calendar/views/pages/index.html')
 })
 
 router.get('/register', (req, res)=>{
