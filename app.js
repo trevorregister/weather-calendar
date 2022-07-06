a8d1f14f61683b4d4714a60c82de008f3a2f3262
trevorregister <trevor.register@pivotinteractives.com>
2022-07-06 00:31:38 -0400

    sends style.css

--- app.js
+++ app.js
@@ -7,17 +7,19 @@ const cookieParser = require('cookie-parser')
 const errorHandler = require('./src/middleware/errorHandler')
 const env = require('./config/env.json')
 const db = require('./config/db')
+const path = require('path')
 
 const app = express()
 
 db.connect('dev')
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'html')
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(morgan('dev'))
 app.get('/', web_routes)
+app.get('/style.css', (req,res)=>{res.sendFile(path.join(__dirname+'/views/pages/style.css'))})
 app.use('/api/users', user_routes)
 app.use(errorHandler.returnError)
 
