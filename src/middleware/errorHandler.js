9cb157586a9c7a7f450921be9ef095c744a61cec
trevorregister <trevor.register@pivotinteractives.com>
2022-07-06 00:30:29 -0400

    errors are console.log'd

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,4 +1,6 @@
+const api404 = require('../utils/errors').api404
 function returnError (err, req, res, next) {
+    console.log(err.statusCode || 500, err.name)
     res.status(err.statusCode || 500).send(err.name)
    }
 
